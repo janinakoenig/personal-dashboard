ff2a36ab366eb7f0a287fc288cc9f65516806119
janinakoenig <koenigjanina02@gmail.com>
2026-01-22 14:11:06 +0100

    components

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import "./Card.css";
+
+export default function Card () {
+    return (
+        <section className="card">
+
+        </section>
+    );
+}
---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import "./Dashboard.css";
+import Sidebar from "../Sidebar/Sidebar.tsx";
+import Card from "../Card/Card.tsx";
+
+export default function Dashboard() {
+    return <h1>Hello</h1>;
+    <div className="appShell">
+        <Card>
+        </Card> 
+        <Sidebar>
+            
+        </Sidebar>
+    </div>
+}
