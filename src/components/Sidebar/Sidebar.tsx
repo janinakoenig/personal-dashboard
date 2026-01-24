b0f5dfc7f8eb3a12171a1f379eeec282cccf6304
janinakoenig <koenigjanina02@gmail.com>
2026-01-24 19:15:31 +0100

    sidebar updates

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,26 @@
 import "./Sidebar.css";
+import porfilePicture from "../../assets/profilepicture.png";
 
 export default function sidebar () {
     return (
         <div className="sidebar">
-            <section className="dashborad">
-                Dashboard
-            </section>
-            <section className="classes">
-                My classes
-            </section>
-            <section className="grades">
-                My grades
+            <div className="profile">
+                <img className="profilepicture" src={porfilePicture}></img>
+                <div className="name">
+                    <span className="firstname">Janina</span>
+                    <span className="lastname">König</span>
+                </div>
+            </div>
+            <section className="menu">
+                        <button className="menuButton classesButton">
+                    My classes
+                </button>
+                <button className="menuButton gradesButton">
+                    My grades
+                </button>
+                <button className="menuButton dashboardButton">
+                    Dashboard
+                </button>
             </section>
         </div>
     );
